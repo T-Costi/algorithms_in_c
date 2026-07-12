bf68f03b3a6bf2a9b41b8ab0fdf825e9876aa7f9
T-COSTI <267923429+T-Costi@users.noreply.github.com>
2026-07-12 15:58:47 +0200

    Add shared test utilities

--- tests/test_bubble_sort.c
+++ tests/test_bubble_sort.c
@@ -1,36 +1,7 @@
 #include <stddef.h>
 #include <stdio.h>
+#include "test_utils.h"
 #include "../sorting/bubble_sort.h"
-
-void print_array(int array[], int len){
-    if (array == NULL || len == 0) {
-        printf("[]\n");
-        return; 
-    }
-    printf("[ ");
-    int i;
-    for (i = 0; i<len; i++){
-        printf("%d", array[i]);
-        if (i < len - 1){
-            printf(", ");
-        }
-    }
-    printf("]\n");
-}
-
-int test_assertion(int array1[], int array2[], int len1, int len2){
-    if (len1!=len2){return 0;}
-    if (array1 == NULL && array2 == NULL ){return 1;}
-    if (array1 == NULL || array2 == NULL ){return 0;}
-    
-    for (int i = 0; i < len1; i++){
-        if(array1[i] != array2[i]){ //checking element by element to see if the arrays are identical
-            return 0;
-        }
-    }
-    return 1;
-}
-
 
 // normal disorganized array
 int test_case1(){
---
+++ tests/test_utils.h
@@ -0,0 +1,7 @@
+#ifndef TEST_UTILS_H
+#define TEST_UTILS_H
+
+void print_array(int array[], int len);
+int test_assertion(int array1[], int array2[], int len1, int len2);
+
+#endif
